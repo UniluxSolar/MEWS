44d3c0a104d116f2f742d4987d1451f08be05eb9
UniluxSolar <office.uniluxsolar@gmail.com>
2026-01-20 13:29:30 +0530

    Fix SyntaxError: Remove duplicate path declaration in server.js

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,6 @@ const express = require('express');
 const cors = require('cors');
 const connectDB = require('./config/db');
 const path = require('path');
-const path = require('path');
 const fs = require('fs');
 const cookieParser = require('cookie-parser');
 
